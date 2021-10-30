8646b9d6f00cdc5e179ba78f77470fede35959de
Rehd Colipano <rgcolipano@gmail.com>
2021-10-30 01:01:02 -0600

    Crypto tracker app using React

---
+++ src/components/Coin/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Coin.css';
+
+const Coin = ({
+  image,
+  name,
+  symbol,
+  price,
+  volume,
+  priceChange,
+  marketcap,
+}) => {
+  return (
+    <>
+      <div className='coin-container'>
+        <div className='coin-row'>
+          <div className='coin'>
+            <img src={image} alt='crypto-image' />
+            <h1>{name}</h1>
+            <p className='coin-symbol'>{symbol}</p>
+          </div>
+          <div className='coin-details'>
+            <p className='coin-price'>${price}</p>
+            <p className='coin-volume'>${volume.toLocaleString()}</p>
+            {priceChange < 0 ? (
+              <p className='down'>{priceChange.toFixed(2)}%</p>
+            ) : (
+              <p className='up'>{priceChange.toFixed(2)}%</p>
+            )}
+            <p className='coin-marketcap'>
+              Market Cap: ${marketcap.toLocaleString()}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Coin;
